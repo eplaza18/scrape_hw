16158976d856949ea21517af47037478df9f9e6c
erikplaza@gmail.com <erikplaza@gmail.com>
2019-05-28 20:34:02 -0700

    added mongodb connect code

--- server.js
+++ server.js
@@ -26,8 +26,10 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scraperHW", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
